917fd2fbbe59c8c4977844d32cd342029153a204
realestate-demo <nehalsharma3785@gmail.com>
2024-08-29 17:54:47 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
 function App() {
+
+  let name = "Hello World!";
+
+  // array
+  // let arr = ['a','b','c','d','e','f']
+  let arr = [12, 41, 4, 5, 82, 5]
+
+  // obj
+  let obj = {
+    'name':"Nehal Dave",
+    'class': "third",
+    'age' : 20,
+    'rank': "first"
+  }
+
+   let status = true;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+        <h1>{name}</h1>
+
+        {/* map for arr need to iterarte each element */}
+        {arr.map((e) => {
+          return (
+            <div>{e}</div>
+          )
+        }
+        )}
+
+        <div>
+        {obj.name}
+        </div>
+
+        <div>{10 *20}</div>
+
+      {/* ternary operator */}
+      {
+      (status) ? <h1 style={{color:"red"}}> Welcome to the page </h1> :  " "}
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
